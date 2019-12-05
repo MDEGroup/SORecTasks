66d172b3fa98c34d51f70a1bbc1cddaff5325ab6
Emanuele Palombo <muttley.bd@gmail.com>
2019-12-05 19:37:03 +0100

    apachePOI

--- apachePOI/src/main/java/sorec/evaluation/task/apachePOI/App.java
+++ apachePOI/src/main/java/sorec/evaluation/task/apachePOI/App.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -32,98 +33,207 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.ss.util.WorkbookUtil;
 
-
 public class App {
 	
-	public HSSFWorkbook writeExcelFile(String filename) {		
-		 HSSFWorkbook workbook = new HSSFWorkbook();
-	        HSSFSheet sheet = workbook.createSheet("Datatypes in Java");
-	      
-	        return workbook;
-	    
-    }
+	/*
+	 * notes: before start the methods implementation I spent 10/15 minutes 
+	 * to fix the pom.xml, AppTest.java and some little errors in the code above. 
+	 */
 	
-	public HSSFWorkbook readExcel(String filename) {	
+	// 18:50 - START SESSION
+	public HSSFWorkbook writeExcelFile(String filename) {
 
-	        try {
+		try {
+			HSSFWorkbook workbook = new HSSFWorkbook();
+			HSSFSheet sheet = workbook.createSheet("Datatypes in Java");
 
-	            FileInputStream excelFile = new FileInputStream(new File(filename));
-	    
-	        } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-			return null;
-	        
-	        
+			FileOutputStream outputStream = new FileOutputStream(filename);
+			workbook.write(outputStream);
+			workbook.close();
+
+			return workbook;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+
 	}
-	
-	
+
+	// 18:52
+	public HSSFWorkbook readExcel(String filename) {
+
+		try {
+
+			FileInputStream excelFile = new FileInputStream(new File(filename));
+			HSSFWorkbook workbook = new HSSFWorkbook(excelFile);
+
+			return workbook;
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	// 19:02
 	public HSSFWorkbook createNewCell(String filename) throws FileNotFoundException, IOException {
-		
-        try (HSSFWorkbook wb = new HSSFWorkbook()) { //or new HSSFWorkbook();
-            CreationHelper creationHelper = wb.getCreationHelper();
-            Sheet sheet = wb.createSheet("new sheet");
 
-        }
-        
-    
+		try (HSSFWorkbook wb = new HSSFWorkbook()) { // or new HSSFWorkbook();
+
+			CreationHelper creationHelper = wb.getCreationHelper();
+
+			Sheet sheet = wb.createSheet("new sheet");
+
+			// Create a row and put some cells in it. Rows are 0 based.
+			Row row = sheet.createRow(0);
+			// Create a cell and put a value in it.
+			Cell cell = row.createCell(0);
+			cell.setCellValue(1);
+
+			// Write the output to a file
+			try (OutputStream fileOut = new FileOutputStream(filename)) {
+				wb.write(fileOut);
+			}
+
+			return wb;
+		}
 	}
-	
+
+	// 19:06
 	public Workbook iterateCell(String filename) throws FileNotFoundException, IOException {
-		 try (
-	                FileInputStream is = new FileInputStream(filename);
-	                Workbook wb = new HSSFWorkbook(is)
-	            ) {
-	          
-	                
-	            
-	            return wb;		 }
-		
+		try (FileInputStream is = new FileInputStream(filename); Workbook wb = new HSSFWorkbook(is)) {
+
+			int i = 0;
+
+			// A che serve iterare se torna il WorkBook?!
+			for (Sheet sheet : wb) {
+				for (Row row : sheet) {
+					for (Cell cell : row) {
+						i++;
+					}
+				}
+			}
+
+			return wb;
+		}
 	}
-	
+
+	// 19:09
 	public Workbook addCellColors(String filename) throws FileNotFoundException, IOException {
-		 try (Workbook wb = new HSSFWorkbook()) { //or new HSSFWorkbook();
-	            Sheet sheet = wb.createSheet("new sheet");
+		try (Workbook wb = new HSSFWorkbook()) { // or new HSSFWorkbook();
+			Sheet sheet = wb.createSheet("new sheet Color");
 
-	            // Create a row and put some cells in it. Rows are 0 based.
-	            Row row = sheet.createRow(1);
+			// Create a row and put some cells in it. Rows are 0 based.
+			Row row = sheet.createRow(1);
 
-	            // Aqua background
-	            CellStyle style = wb.createCellStyle();
-	        
-	        }
+			// Aqua background
+			CellStyle style = wb.createCellStyle();
+			
+			style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+			style.setFillPattern(FillPatternType.BIG_SPOTS);
+
+			Cell cell = row.createCell(1);
+			cell.setCellValue("X");
+			cell.setCellStyle(style);
+
+			// Write the output to a file
+			try (OutputStream fileOut = new FileOutputStream(filename)) {
+				wb.write(fileOut);
+			}
+
+			return wb;
+		}
 	}
-	
-	
-	
-	
-	
+
+	// 19:11
 	public HSSFWorkbook createNewSheet(String filename) throws IOException {
-		
-        }
+		try (HSSFWorkbook wb = new HSSFWorkbook()) { // or new HSSFWorkbook();
+
+			Sheet sheet = wb.createSheet("new sheet x");
+
+			// Write the output to a file
+			try (OutputStream fileOut = new FileOutputStream(filename)) {
+				wb.write(fileOut);
+			}
+
+			return wb;
+		}
+
 	}
-	
+
+	// 19:12
 	public Workbook commentsCell(String filename) throws IOException {
-	     try (Workbook wb = new HSSFWorkbook()) {
+		try (Workbook wb = new HSSFWorkbook()) {
 
-	            CreationHelper factory = wb.getCreationHelper();
+			CreationHelper factory = wb.getCreationHelper();
 
-	            
+			Sheet sheet = wb.createSheet();
+
+			Row row = sheet.createRow(3);
+			Cell cell = row.createCell(5);
+
+			cell.setCellValue("F4");
+
+			Drawing drawing = sheet.createDrawingPatriarch();
+
+			// When the comment box is visible, have it show in a 1x3 space
+			ClientAnchor anchor = factory.createClientAnchor();
+			anchor.setCol1(cell.getColumnIndex());
+			anchor.setCol2(cell.getColumnIndex() + 1);
+			anchor.setRow1(row.getRowNum());
+			anchor.setRow2(row.getRowNum() + 3);
+
+			// Create the comment and set the text+author
+			Comment comment = drawing.createCellComment(anchor);
+			RichTextString str = factory.createRichTextString("Hello, World!");
+
+			comment.setString(str);
+			comment.setAuthor("Apache POI");
+
+			// Assign the comment to the cell
+			cell.setCellComment(comment);
+
+			try (OutputStream out = new FileOutputStream(filename)) {
+				wb.write(out);
+			}
+			wb.close();
+
+			return wb;
+		}
 	}
-	
+
+	// 19:16
 	public HSSFWorkbook addCellDate(String filename) throws FileNotFoundException, IOException {
-        try (HSSFWorkbook wb = new HSSFWorkbook()) {
-            HSSFSheet sheet = wb.createSheet("new sheet");
-            
-            HSSFRow row = sheet.createRow(0);
+		try (HSSFWorkbook wb = new HSSFWorkbook()) {
+			HSSFSheet sheet = wb.createSheet("new sheet");
 
-           
-        }
-    }
-		
-	
-	
+			HSSFRow row = sheet.createRow(0);
 
+			CreationHelper createHelper = wb.getCreationHelper();
+
+			Cell cell = row.createCell(0);
+
+			cell.setCellValue(new Date());
+
+			CellStyle cellStyle = wb.createCellStyle();
+			cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
+			cell = row.createCell(1);
+			cell.setCellValue(new Date());
+			cell.setCellStyle(cellStyle);
+
+			// Write the output to a file
+			try (OutputStream fileOut = new FileOutputStream(filename)) {
+				wb.write(fileOut);
+			}
+
+			return wb;
+		}
+	}
+
+	// 19:17 END SESSION
+	// note: all info for writing code, come from: https://poi.apache.org/components/spreadsheet/quick-guide.html
 }
--- apachePOI/src/test/java/sorec/evaluation/task/apachePOI/AppTest.java
+++ apachePOI/src/test/java/sorec/evaluation/task/apachePOI/AppTest.java
@@ -23,42 +23,43 @@ public class AppTest {
 	
 	@Test
 	public void writeExcelTest() {
-		assertNotNull(app.writeExcelFile("C://Users//claudio//Desktop//excel.xls"));
+		assertNotNull(app.writeExcelFile("./excel.xls"));
 	}
 	@Test
 	public void readExcelFileTest() {
-		assertNotNull(app.readExcel("C://Users//claudio//Desktop//excel.xls"));
+		assertNotNull(app.readExcel("./excel.xls"));
 	}
 	@Test
 	public void createNewCellTest() throws FileNotFoundException, IOException {
-		assertNotNull(app.createNewCell("C://Users//claudio//Desktop//excel.xls"));
+		assertNotNull(app.createNewCell("./excel.xls"));
 	}
 	@Test
 	public void iterateCellTest() throws FileNotFoundException, IOException {
-		assertNotEquals(app.iterateCell("C://Users//claudio//Desktop//excel.xls").getNumberOfSheets(),0);
+		assertNotEquals(app.iterateCell("./excel.xls").getNumberOfSheets(),0);
 	}
 
 	@Test
 	public void addCellColorsTest() throws IOException {
-		assertNotNull(app.addCellColors("C://Users//claudio//Desktop//excel.xls"));
+		assertNotNull(app.addCellColors("./excel.xls"));
 	}
+	
 	@Test
 	public void createNewSheetTest() throws IOException {
-		HSSFWorkbook wb = app.readExcel("C://Users//claudio//Desktop//excel.xls");
+		HSSFWorkbook wb = app.readExcel("./excel.xls");
 		int begin=wb.getNumberOfSheets();		
-		app.createNewSheet("C://Users//claudio//Desktop//excel.xls");
+		app.createNewSheet("./excel.xls");
 		int end=wb.getNumberOfSheets();		
 		assertEquals(begin, end);
 	}
 	
 	@Test
 	public void commentsCellTest() throws IOException {
-		assertNotNull(app.commentsCell("C://Users//claudio//Desktop//excel.xls"));
+		assertNotNull(app.commentsCell("./excel.xls"));
 	}
 	
 	@Test
 	public void addCellDateTest() throws IOException {
-		assertNotNull(app.addCellDate("C://Users//claudio//Desktop//excel.xls"));
+		assertNotNull(app.addCellDate("./excel.xls"));
 	}
 	
 	
