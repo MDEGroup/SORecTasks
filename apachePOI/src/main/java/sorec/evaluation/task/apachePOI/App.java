a51df6b9811a5b6ad2b9b20c0262b8dd3f554086
Admin <incristi@gmail.com>
2019-12-15 15:21:29 +0100

    //Cristina Stratan

--- apachePOI/src/main/java/sorec/evaluation/task/apachePOI/App.java
+++ apachePOI/src/main/java/sorec/evaluation/task/apachePOI/App.java
@@ -31,99 +31,171 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.ss.util.WorkbookUtil;
-
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class App {
-	
-	public HSSFWorkbook writeExcelFile(String filename) {		
-		 HSSFWorkbook workbook = new HSSFWorkbook();
-	        HSSFSheet sheet = workbook.createSheet("Datatypes in Java");
-	      
-	        return workbook;
-	    
-    }
-	
-	public HSSFWorkbook readExcel(String filename) {	
 
-	        try {
+	public HSSFWorkbook writeExcelFile(String filename) {
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet("Datatypes in Java");
 
-	            FileInputStream excelFile = new FileInputStream(new File(filename));
-	    
-	        } catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-			return null;
-	        
-	        
+		return workbook;
+
 	}
-	
-	
-	public HSSFWorkbook createNewCell(String filename) throws FileNotFoundException, IOException {
-		
-        try (HSSFWorkbook wb = new HSSFWorkbook()) { //or new HSSFWorkbook();
-            CreationHelper creationHelper = wb.getCreationHelper();
-            Sheet sheet = wb.createSheet("new sheet");
 
-        }
-        
-    
+	// 10min
+	public HSSFWorkbook readExcel(String filename) {
+
+		try {
+			FileInputStream excelFile = new FileInputStream(filename);
+			Workbook workbook = new XSSFWorkbook(excelFile);
+			Sheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+
+			while (iterator.hasNext()) {
+
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+
+				while (cellIterator.hasNext()) {
+
+					Cell currentCell = cellIterator.next();
+					if (currentCell.getCellTypeEnum() == CellType.STRING) {
+						System.out.print(currentCell.getStringCellValue() + "--");
+					} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+						System.out.print(currentCell.getNumericCellValue() + "--");
+					}
+
+				}
+				System.out.println();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+
 	}
-	
+
+	// 7 min
+	public HSSFWorkbook createNewCell(String filename, Integer rowNo, Integer cellNo)
+			throws FileNotFoundException, IOException {
+
+		try (HSSFWorkbook wb = new HSSFWorkbook()) {
+			CreationHelper creationHelper = wb.getCreationHelper();
+			Sheet sheet = wb.createSheet("new sheet");
+			Row row = sheet.createRow(rowNo);
+			Cell cell = row.createCell(cellNo);
+			try (FileOutputStream outputStream = new FileOutputStream(filename)) {
+				workbook.write(outputStream);
+			}
+			return workbook;
+
+		}
+	}
+
+	// 7min
 	public Workbook iterateCell(String filename) throws FileNotFoundException, IOException {
-		 try (
-	                FileInputStream is = new FileInputStream(filename);
-	                Workbook wb = new HSSFWorkbook(is)
-	            ) {
-	          
-	                
-	            
-	            return wb;		 }
-		
+		try (FileInputStream is = new FileInputStream(filename); Workbook wb = new HSSFWorkbook(is)) {
+
+			for (Sheet sheet : wb) {
+				for (Row row : sheet) {
+					for (Cell cell : row) {
+
+					}
+				}
+			}
+
+			return wb;
+		}
+
 	}
-	
+
+	// 10min
 	public Workbook addCellColors(String filename) throws FileNotFoundException, IOException {
-		 try (Workbook wb = new HSSFWorkbook()) { //or new HSSFWorkbook();
-	            Sheet sheet = wb.createSheet("new sheet");
+		try (Workbook wb = new HSSFWorkbook()) { // or new HSSFWorkbook();
+			Sheet sheet = wb.createSheet("new sheet");
 
-	            // Create a row and put some cells in it. Rows are 0 based.
-	            Row row = sheet.createRow(1);
+			// Create a row and put some cells in it. Rows are 0 based.
+			Row row = sheet.createRow(1);
+			Cell cell = row.createCell(0);
+			cell.setCellValue(1);
 
-	            // Aqua background
-	            CellStyle style = wb.createCellStyle();
-	        
-	        }
+			// Aqua background
+			CellStyle style = wb.createCellStyle();
+
+			style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+			cell.setCellStyle(style);
+			try (FileOutputStream outputStream = new FileOutputStream(filename)) {
+				workbook.write(outputStream);
+
+			}
+			return workbook;
+		}
 	}
-	
-	
-	
-	
-	
+
+	// 3min
 	public HSSFWorkbook createNewSheet(String filename) throws IOException {
-		
-        }
+		FileInputStream excelFile = new FileInputStream(filename);
+		Workbook workbook = new XSSFWorkbook(excelFile);
+		Sheet sheet = workbook.createSheet("New Sheet");
+		try (FileOutputStream outputStream = new FileOutputStream(filename)) {
+			workbook.write(outputStream);
+
+		}
+		return workbook;
 	}
-	
+
+	}
+
+//10min
 	public Workbook commentsCell(String filename) throws IOException {
-	     try (Workbook wb = new HSSFWorkbook()) {
-
+	     try (Workbook workbook = new HSSFWorkbook()) {
+	    	 Sheet sheet = workbook.createSheet();
+	         Drawing drawing = sheet.createDrawingPatriarch();
 	            CreationHelper factory = wb.getCreationHelper();
+	            ClientAnchor anchor = factory.createClientAnchor();
+	            anchor.setCol1(cell.getColumnIndex());
+	            anchor.setCol2(cell.getColumnIndex()+1);
+	            anchor.setRow1(row.getRowNum());
+	            anchor.setRow2(row.getRowNum()+3);
+	            
+	            Comment c = drawing.createCellComment(anchor);
+	            RichTextString str = factory.createRichTextString(comment);
+	            c.setString(str);
 
+	            cell.setCellComment(c);
+	            
+	            try (FileOutputStream outputStream = new FileOutputStream(filename)) {
+	              	 workbook.write(outputStream);
+	              	
+	       	 }
+	       	return workbook;
 	            
 	}
-	
-	public HSSFWorkbook addCellDate(String filename) throws FileNotFoundException, IOException {
-        try (HSSFWorkbook wb = new HSSFWorkbook()) {
-            HSSFSheet sheet = wb.createSheet("new sheet");
-            
-            HSSFRow row = sheet.createRow(0);
 
-           
-        }
-    }
-		
-	
-	
+	// 6min
+	public HSSFWorkbook addCellDate(String filename) throws FileNotFoundException, IOException {
+		try (HSSFWorkbook workbook = new HSSFWorkbook()) {
+			HSSFSheet sheet = workbook.createSheet("new sheet");
+
+			HSSFRow row = sheet.createRow(0);
+			Cell cell = row.createCell(0);
+			cell.setCellValue(new Date());
+			
+			CellStyle cellStyle = workbook.createCellStyle();
+			CreationHelper createHelper = workbook.getCreationHelper();
+			cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
+			cell.setCellValue(new Date());
+			cell.setCellStyle(cellStyle);
+
+			try (FileOutputStream outputStream = new FileOutputStream(filename)) {
+				workbook.write(outputStream);
+
+			}
+			return workbook;
+		}
+	}
 
 }
--- mongodb/src/main/java/sorec/evaluation/task/mongodb/App.java
+++ mongodb/src/main/java/sorec/evaluation/task/mongodb/App.java
@@ -10,73 +10,156 @@ import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+
+import sorectstudent1.sorecs1.BasicDBObject;
+import sorectstudent1.sorecs1.DBObject;
+
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.gt;
 import static com.mongodb.client.model.Filters.lt;
+
 /**
  * Hello world!
  *
  */
 public class App {
+	// no Sorec
+	// Google: 4min
 	public List<Document> findAll(String collectionName) {
 		List<Document> documents = new ArrayList<Document>();
 		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
 			MongoDatabase database = mongoClient.getDatabase("CROSSMINER");
-			
+			documents = (List<Document>) database.listCollections();
 		} catch (Exception e) {
-		
+
 		}
 		return documents;
 	}
 
+	// 10min (no SOREC)
 	public Document findOneByFullName(String collectionName, String fullName) {
-		return null;
+		Document cursor = new Document();
+		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			MongoCollection<Document> collection = db.getCollection(collectionName);
+
+			BasicDBObject searchQuery = new BasicDBObject();
+			searchQuery.put(fullName);
+
+			cursor = collection.findOne(searchQuery).iterator();
+		} catch (Exception e) {
+
+		}
+		return cursor;
 	}
 
+	// 10min without Sorec
 	public Document updateDocument(String collectionName, String fullName) {
+		Document cursor = new Document();
+		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			BasicDBObject document = new BasicDBObject();
+			document.put(fullName);
+			db.getCollection(collectionName).update(document);
+		} catch (Exception e) {
+
+		}
 		return null;
 	}
 
+	// 20min with Sorec + google
 	public boolean deleteDocument(String collectionName, String fullName) {
-		
+		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			BasicDBObject document = new BasicDBObject();
+			document.put(fullName);
+			db.getCollection(collectionName).deleteOne(document);
+			return true;
+		} catch (Exception e) {
+		}
+		return false;
+
 	}
 
-	public Collection<Document> filterQuery(String collectionName, String field) {
-		 
+	// 15min with Sorec
+	public Collection filterQuery(String collectionName, String field) {
+		Collection cursor = new Collection();
+
+		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			MongoCollection<Document> collection = db.getCollection(collectionName);
+
+			BasicDBObject searchQuery = new BasicDBObject();
+			searchQuery.put(field, "value");
+
+			cursor = collection.find(searchQuery).iterator();
+		} catch (Exception e) {
+		}
+		return cursor;
+
 	}
-	
+
+	// 20min with Sorec
 	public BsonDocument BsonFilter(String collectionName) {
 		Bson filter = and(eq("field1", "value"), gt("field2", "value2"));
 		BsonDocument bsonDocument = filter.toBsonDocument(BsonDocument.class, MongoClient.getDefaultCodecRegistry());
 
-	}
-	
-	public MongoCollection<Document>  insertSingleDocument(String collectionName) {
 		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
-			
-		      
-		        
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			MongoCollection<Document> collection = db.getCollection(collectionName);
+			bsonDocument = (BsonDocument) collection.find(filter).iterator();
+		} catch (Exception e) {
+		}
+		return bsonDocument;
+	}
+
+	// 8min without Sorec
+	public MongoCollection<Document> insertSingleDocument(String collectionName) {
+		MongoCollection<Document> doc;
+
+		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			MongoCollection<Document> collection = db.getCollection(collectionName);
+
+			BasicDBObject document = new BasicDBObject();
+			document.put("name", "lokesh");
+
+			doc = collection.insert(document);
 		} catch (Exception e) {
 			System.err.println("Error in query the collection " + collectionName);
 			throw e;
 		}
-		
+		return doc;
 	}
+
+	// 5min without Sorec
 	public MongoCollection<Document> insertMultipleDocument(String collectionName) {
+		MongoCollection<Document> doc;
+
 		try (MongoClient mongoClient = new MongoClient("localhost", 27017)) {
-			
-	 
-	        
+			MongoDatabase db = mongoClient.getDatabase("CROSSMINER");
+			MongoCollection<Document> collection = db.getCollection(collectionName);
+			DBObject document1 = new BasicDBObject();
+			document1.put("name", "Kiran");
+			document1.put("age", 20);
+
+			DBObject document2 = new BasicDBObject();
+			document2.put("name", "John");
+
+			List<DBObject> documents = new ArrayList<>();
+			documents.add(document1);
+			documents.add(document2);
+			doc = collection.insert(documents);
 		} catch (Exception e) {
 			System.err.println("Error in query the collection " + collectionName);
 			throw e;
 		}
+		return doc;
 	}
-	
+
 }
